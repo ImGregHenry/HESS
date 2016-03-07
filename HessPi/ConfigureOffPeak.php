a83c39c095b34a99c6dc4e6a43ec65774877cdb1
Ajanthi Vasantharoopan <ajanthi.v@gmail.com>
2016-03-06 22:08:50 -0500

    execution order updated

--- HessPi/ConfigureOffPeak.php
+++ HessPi/ConfigureOffPeak.php
@@ -2,8 +2,8 @@
     
     exec('sudo python /pythonscripts/ACFromWallOn.py');
     sleep(1);
-    exec('sudo python /pythonscripts/BatteryChargerOn.py');
-    sleep(1);
     exec('sudo python /pythonscripts/InverterToggle.py');
+    sleep(1);
+    exec('sudo python /pythonscripts/BatteryChargerOn.py');
     sleep(1);
 ?>
