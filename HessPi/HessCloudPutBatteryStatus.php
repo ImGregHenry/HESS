f1a6e40e7a3d762b80fdda3bee7791c09ce05da9
Ajanthi Vasantharoopan <ajanthi.v@gmail.com>
2016-03-06 20:34:28 -0500

    send battery status and update table working

--- HessPi/HessCloudPutBatteryStatus.php
+++ HessPi/HessCloudPutBatteryStatus.php
@@ -1,18 +1,15 @@
 <?php
-include 'HessGlobals.php';
+include_once 'HessGlobals.php';
 
 $result = json_decode(file_get_contents("php://input"));
-echo '<pre>'.print_r($result,1).'</pre>';
+$item = $result->BatteryStatus;
 
-foreach($result->BatteryStatus as $item) {
+
 		
 	$peakScheduleID = $item->PeakScheduleID;
 	$isEnabled = $item->IsEnabled;
 	$recordTime =  strtotime($item->RecordTime);
-	//$recordTimeMS = $item->RecordTimeMS;
-	$powerLevelValue = $item->PowerLevelValue;
 	$powerLevelPercent = $item->PowerLevelPercent;
-    $deviceID = $item->DeviceID;
 	
 	$cloudRecordTime = DATE(DB_DATE_FORMAT, TIME());
 	//$cloudRecordTimeMS = substr((string)microtime(), 2, 3);
@@ -21,21 +18,21 @@ foreach($result->BatteryStatus as $item) {
 
 	try {
 		
-		$query = "INSERT INTO BatteryStatus (PeakScheduleID, DeviceID, IsEnabled, RecordTime, CloudRecordTime, PowerLevelValue, PowerLevelPercent) " //RecordTimeMS, CloudRecordTimeMS, 
-			. "VALUES (:peakScheduleID, :deviceID, :isEnabled, :recordTime, :cloudRecordTime, :powerLevelValue, :powerLevelPercent)"; //:recordTimeMS, :cloudRecordTimeMS, 
+		$query = "INSERT INTO BatteryStatus (PeakScheduleID, IsEnabled, RecordTime, CloudRecordTime, PowerLevelPercent) " //RecordTimeMS, CloudRecordTimeMS,
+			. "VALUES (:peakScheduleID, :isEnabled, :recordTime, :cloudRecordTime, :powerLevelPercent)"; //:recordTimeMS, :cloudRecordTimeMS,
 		
-		$conn = new PDO("mysql:host=MYSQL_CLOUD_HOST;dbname=MYSQL_CLOUD_DATABASE", MYSQL_CLOUD_USER, MYSQL_CLOUD_PASSWORD);
+		$conn = new PDO("mysql:host=" . MYSQL_CLOUD_HOST . ";dbname=" . MYSQL_CLOUD_DATABASE, MYSQL_CLOUD_USER, MYSQL_CLOUD_PASSWORD);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		
 		$stmt = $conn->prepare($query);
 		$stmt->bindParam(':peakScheduleID', $peakScheduleID, PDO::PARAM_STR, 20);
-		$stmt->bindParam(':deviceID', $deviceID, PDO::PARAM_INT);
+		//$stmt->bindParam(':deviceID', $deviceID, PDO::PARAM_INT);
 		$stmt->bindParam(':isEnabled', $isEnabled, PDO::PARAM_INT);
 		$stmt->bindParam(':recordTime', date(DB_DATE_FORMAT, $recordTime), PDO::PARAM_STR);
 		//$stmt->bindParam(':recordTimeMS', $recordTimeMS, PDO::PARAM_INT);
 		$stmt->bindParam(':cloudRecordTime', $cloudRecordTime, PDO::PARAM_STR);
 		//$stmt->bindParam(':cloudRecordTimeMS', $cloudRecordTimeMS, PDO::PARAM_INT);
-		$stmt->bindParam(':powerLevelValue', floatval($powerLevelValue), PDO::PARAM_STR);
+		//$stmt->bindParam(':powerLevelValue', floatval($powerLevelValue), PDO::PARAM_STR);
 		$stmt->bindParam(':powerLevelPercent', floatval($powerLevelPercent), PDO::PARAM_STR);
 	
 		$stmt->execute();
@@ -45,7 +42,6 @@ foreach($result->BatteryStatus as $item) {
 	{
 		echo "ERROR: $e";
         return;
-	}	
-}
+
 
 ?>
--- HessPi/HessPiSendBatteryStatus.php
+++ HessPi/HessPiSendBatteryStatus.php
@@ -1,43 +1,42 @@
 <?php
     
-	$percent = 0;
-	
-	$url = "http://hess.site88.net/HessCloudPutBatteryStatus.php";
+    $percent = 0;
     
-	$ch = curl_init( $url );
-	
-	//TODO: get the actual current PeakScheduleID
-
+    $BatteryStatus = array("BatteryStatus" => array());
+    
+    $url = "http://hess.site88.net/HessCloudPutBatteryStatus.php";
+    
+    $ch = curl_init( $url );
+    
+    //TODO: get the actual current PeakScheduleID
+    
     # Set time zone to get proper time
     date_default_timezone_set('US/Eastern');
     $DATE_FORMAT = "Y-m-d H:i:s";
     $timestamp = DATE($DATE_FORMAT, TIME());
-	//$timestampMS = round(microtime(true) * 1000);
-	
-	# Setup request to send json via POST.
-	$jsonme = array(
-					"BatteryStatus" => array(
-											 array(
-												   'PeakScheduleID' => 1,
-												   'IsEnabled' => 1,
-												   'PowerLevelPercent' => $percent++,
-												   'RecordTime' => $timestamp,
-												   'DeviceID' => 19
-												   )
-											 )
-					);
-	
-	$payload = json_encode( $jsonme );
-	curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
-	curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-	
-	# Send request.
-	$result = curl_exec($ch);
-	curl_close($ch);
-	//echo "TIME SENT : " . $timestamp . "\n"; 
-
-	echo "JSON Package Sent <pre>$payload<pre>";
-	//sleep(3);
-	//}
+    //$timestampMS = round(microtime(true) * 1000);
+    
+    # Setup request to send json via POST.
+    $temparray = array('PeakScheduleID' => 1,
+                       'IsEnabled' => 1,
+                       'PowerLevelPercent' => 80,
+                       'RecordTime' => $timestamp);
+    
+    array_push($BatteryStatus['BatteryStatus'], $temparray);
+    
+    $payload = json_encode( $BatteryStatus );
+    curl_setopt( $ch, CURLOPT_POST, true );
+    curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
+    curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+    
+    # Send request.
+    $result = curl_exec($ch);
+    echo $result;
+    curl_close($ch);
+    //echo "TIME SENT : " . $timestamp . "\n";
+    
+    echo "<pre>$payload<pre>";
+    //sleep(3);
+    //}
 ?>
