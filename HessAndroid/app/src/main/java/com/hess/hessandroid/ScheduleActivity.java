a0db8ac8b7bbd4bcbe50cfbaabf6cb0a12c35137
Greg Henry <imsupergreg@live.com>
2016-03-11 15:42:33 -0500

    Fixed gitignore bug where .xml files were ignored
    - Fixed .gitignore bug where all of our .xml files were being ignored
    unless they were force added.

---
+++ HessAndroid/app/src/main/java/com/hess/hessandroid/ScheduleActivity.java
@@ -0,0 +1,19 @@
+package com.hess.hessandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
