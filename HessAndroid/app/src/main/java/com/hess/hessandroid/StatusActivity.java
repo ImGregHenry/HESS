e9ef1f77314fe5f1f80fd3a8f7e5d2d5b3f65f43
Greg Henry <imsupergreg@live.com>
2016-03-11 13:47:47 -0500

    Old code removal.  Git ignore fixes.
    - More git ignore fixes.
    - Added missing file from previous checkin.

--- HessAndroid/app/src/main/java/com/hess/hessandroid/StatusActivity.java
+++ HessAndroid/app/src/main/java/com/hess/hessandroid/StatusActivity.java
@@ -1,6 +1,7 @@
 package com.hess.hessandroid;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -25,6 +26,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import com.hess.hessandroid.models.StatusModel;
+import com.hess.hessandroid.volley.VolleyRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -44,17 +46,21 @@ public class StatusActivity extends AppCompatActivity {
     Handler mHandler = new Handler();
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
-        tv = (TextView)findViewById(R.id.tvTest);
+        tv = (TextView) findViewById(R.id.tvTest);
 
+        VolleyRequest vr = new VolleyRequest();
+        //vr.getData(this);
+        vr.postData(this, "", new JSONObject());
 
         Log.d(LOG_STRING, "Starting Session.  Max Messages: " + MAX_MESSAGES + ".");
-tv.setText("THIS");
-        //initialize();
+        tv.setText("THIS");
+
+    }
+    //initialize();
 //
 //        for(int i = 0; i < MAX_MESSAGES; i++) {
 //            try {
@@ -65,91 +71,28 @@ tv.setText("THIS");
 //
 //            }
 //        }
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                // TODO Auto-generated method stub
-                while (true) {
-                    try {
-                        Thread.sleep(2000);
-                        mHandler.post(new Runnable() {
-
-                            @Override
-                            public void run() {
-                                // TODO Auto-generated method stub
-                                getData();                            }
-                        });
-                    } catch (Exception e) {
-                        // TODO: handle exception
-                    }
-                }
-            }
-        }).start();
-//        getArrayData();
-    }
-
-
-    java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    java.text.SimpleDateFormat sdfMS = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-
-    //    StatusModel list;
-    public void getData() {
-        String url = "http://hess.site88.net/HESSGetBatteryStatus.php";
-
-
-        JsonObjectRequest jsonRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                    // the response is already constructed as a JSONObject!
-                        try {
-
-                            JSONArray result = response.getJSONArray("BatteryStatus");
-                            String recordTime = result.getJSONObject(0).getString("RecordTime");
-                            String cloudRecordTime = result.getJSONObject(0).getString("CloudRecordTime");
-                            String cloudRecordTimeMS = result.getJSONObject(0).getString("CloudRecordTimeMS");
-                            String recordTimeMS = result.getJSONObject(0).getString("RecordTimeMS");
-
-
-                            Date piTime = sdfMS.parse(recordTime + "." + recordTimeMS);
-                            Date cloudTime = sdfMS.parse(cloudRecordTime + "." + cloudRecordTimeMS);
-                            Date mobileTime = new Date();
-
-
-                            TimeUnit unit = TimeUnit.MILLISECONDS;
-                            long piToCloud = cloudTime.getTime() - piTime.getTime();
-                            long cloudToMobile = Math.abs(mobileTime.getTime() - cloudTime.getTime());
-
-                            Log.d(LOG_STRING, piToCloud + "," + cloudToMobile);
-
-//                            Log.d(LOG_STRING, "MOBILE: " + mobileTime.getTime());
-                        }
-                        catch(JSONException e) {
-//                            e.printStackTrace();
-                            Log.d(LOG_STRING, "SKIPPED, SKIPPED");
-
-                        }
-                        catch(Exception e) {
-//                            e.printStackTrace();
-                            Log.d(LOG_STRING, "SKIPPED, SKIPPED");
-
-                        }
-//                        Gson gson = new Gson();
-//                        StatusModel arrayList = new StatusModel();
-//                        Type listType = new TypeToken<StatusModel>() { }.getType();
-//                        list = gson.fromJson(response.toString(), listType);
-//                        System.out.println("RESULT: " + list.batteryStatus.get(0).RecordTime);
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        error.printStackTrace();
-                    }
-                });
-
-        Volley.newRequestQueue(this).add(jsonRequest);
-    }
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                // TODO Auto-generated method stub
+//                while (true) {
+//                    try {
+//                        Thread.sleep(2000);
+//                        mHandler.post(new Runnable() {
+//
+//                            @Override
+//                            public void run() {
+//                                // TODO Auto-generated method stub
+//                                getData();                            }
+//                        });
+//                    } catch (Exception e) {
+//                        // TODO: handle exception
+//                    }
+//                }
+//            }
+//        }).start();
+////        getArrayData();
+//    }
 
 
     @Override
